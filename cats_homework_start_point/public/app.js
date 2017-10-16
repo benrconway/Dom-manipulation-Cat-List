10ec42ca4c2b15cde1086028c5f61d3e46928640
Benjamin Conway <benrconway84@gmail.com>
2017-10-16 16:35:29 +0100

    Further code to construct from the end

--- cats_homework_start_point/public/app.js
+++ cats_homework_start_point/public/app.js
@@ -1,20 +1,38 @@
 var
 
 var createUnorderedList = function() {
-  var newCatDetailsList = 
+  var newCatDetailsList = document.createElement("ul");
+  newCatDetailsList.classList.add("cat");
+  return newCatDetailsList;
 }
 
-var createNameListItem
+var createNameListItem = function(name) {
+  var nameListItem = document.createElement("li");
+  nameListItem.textContent = name;
+  return nameListItem;
+}
 
-var createFoodListItem
+var createFoodListItem = function(favouriteFood) {
+  var foodListItem = document.createElement("li");
+  foodListItem.textContent = "Favourite Food: " + favouriteFood;
+}
 
-var createImageListItem
+var createImageListItem = function(image) {
+  var imageListItem = document.createElement("li");
+  var imageItem = document.createElement("img");
+  imageItem.src = image;
+  imageItem.width = 500;
+  imageListItem.appendChild(imageItem);
+  return imageListItem;
+}
 
-var appendChildren
 
 var addCatListItem(name, favouriteFood, image)
 
 
+var appendChildren = function (cats, catName, catFavouriteFood, catImage) {
+
+}
 
 var app = function () {
   for(var cat in cats){
