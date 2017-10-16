d11c4c78eec537225b03249fd19a267593054acf
Benjamin Conway <benrconway84@gmail.com>
2017-10-16 17:02:40 +0100

    works in simplicity

--- cats_homework_start_point/public/app.js
+++ cats_homework_start_point/public/app.js
@@ -1,4 +1,9 @@
-var
+var catInputArray = [
+  { name: "Megatron",
+    favouriteFood: "Enchiladas",
+    image: "http://moderncat.com/sites/default/files/styles/466-wide/public/images/photoentries/photos/wpSpUav.jpg?itok=adE0c33C"
+  }
+]
 
 var createUnorderedList = function() {
   var newCatDetailsList = document.createElement("ul");
@@ -8,13 +13,14 @@ var createUnorderedList = function() {
 
 var createNameListItem = function(name) {
   var nameListItem = document.createElement("li");
-  nameListItem.textContent = name;
+  nameListItem.textContent = "Name: " + name;
   return nameListItem;
 }
 
 var createFoodListItem = function(favouriteFood) {
   var foodListItem = document.createElement("li");
   foodListItem.textContent = "Favourite Food: " + favouriteFood;
+  return foodListItem
 }
 
 var createImageListItem = function(image) {
@@ -27,17 +33,53 @@ var createImageListItem = function(image) {
 }
 
 
-var addCatListItem(name, favouriteFood, image)
-
-
-var appendChildren = function (cats, catName, catFavouriteFood, catImage) {
-
+var appendChildren = function (cats, catBorder, catName, catFavouriteFood, catImage) {
+  catBorder.appendChild(catName);
+  catBorder.appendChild(catFavouriteFood);
+  catBorder.appendChild(catImage);
+  cats.appendChild(catBorder);
 }
+
+var addCatListItem = function(name, favouriteFood, image) {
+  var catBorder = createUnorderedList();
+  var catName = createNameListItem(name);
+  var catFavFood = createFoodListItem(favouriteFood);
+  var catImage = createImageListItem(image);
+  var catCollection = document.querySelector("section#cats")
+  appendChildren(catCollection, catBorder, catName, catFavFood, catImage)
+}
+
+// var makeNewCat = function(){
+//   var name = "Megatron";
+//   var favouriteFood = "Enchiladas"
+//   var image = "http://moderncat.com/sites/default/files/styles/466-wide/public/images/photoentries/photos/wpSpUav.jpg?itok=adE0c33C"
+//
+//   var newCatDetailsList = document.createElement("ul");
+//   newCatDetailsList.classList.add("cat");
+//
+//   var nameListItem = document.createElement("li");
+//   nameListItem.textContent = "Name: " + name;
+//
+//   var foodListItem = document.createElement("li");
+//   foodListItem.textContent = "Favourite Food: " + favouriteFood;
+//
+//   var imageListItem = document.createElement("li");
+//   var imageItem = document.createElement("img");
+//   imageItem.src = "http://moderncat.com/sites/default/files/styles/466-wide/public/images/photoentries/photos/wpSpUav.jpg?itok=adE0c33C";
+//   imageItem.width = 500;
+//
+//   newCatDetailsList.appendChild(nameListItem);
+//   newCatDetailsList.appendChild(foodListItem);
+//   newCatDetailsList.appendChild(imageListItem);
+//   cats.appendChild(newCatDetailsList);
+// }
+
 
 var app = function () {
-  for(var cat in cats){
+  for(var cat in catInputArray){
     addCatListItem(cat.name, cat.favouriteFood, cat.image)
-  }
+    console.dir(catInputArray);
+
 }
 
-window.addEventListener('DMContentLoaded', app);
+window.addEventListener('DOMContentLoaded', app);
